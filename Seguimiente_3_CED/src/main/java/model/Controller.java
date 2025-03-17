4c3925ff4540e7e337bad7177cae8584a606f70a
JuanPaArevalo <juan.parevalo3@gmail.com>
2025-03-17 12:27:23 -0500

    Analisis de complejidad - Alg1

--- Seguimiente_3_CED/src/main/java/model/Controller.java
+++ Seguimiente_3_CED/src/main/java/model/Controller.java
@@ -9,16 +9,34 @@ public class Controller {
         arrNum = new int[]{1, 2, 2, 3, 4, 4, 4, 5, 6, 6};
     }
 
-    public String orderValues() {
-        List<Integer> duplicates = new ArrayList<>(); //1
+    public List<Integer> orderValues() {
 
-        for (int i = 0; i < arrNum.length - 1; i++) {
-            if (arrNum[i] == arrNum[i + 1] && (duplicates.isEmpty() || duplicates.get(duplicates.size() - 1) != arrNum[i])) { //n veces
-                duplicates.add(arrNum[i]);
+        List<Integer> duplicates = new ArrayList<>(); // Se repite 1 vez
+
+        /* El peor caso es cuando tenemos una lista asi : [1,1,2,2,3,3,4,4...], ya que de esta manera
+        hacemos que el programa tenga que hacer el mayor número de operaciones, por que el cuerpo del if se ejecutaria n/2 aproximadamente,
+        que es el número más grande de veces que el programa podria ejecutar esta linea. */
+
+        for (int i = 0; i < arrNum.length - 1 ; i++) { /*Se repite n veces ( n = longitud del arreglo ). Se repite n veces porque la i empieza en 0
+            y como el ciclo se va a repetir mientras i sea menor que n - 1 se repetiria n - 1 veces, pero como el programa tiene que comprobar una ultima vez
+            la condición, entonces se agrega una ultima ejecución a n - 1, es decir, n-1+1 = n. */
+
+            if (arrNum[i] == arrNum[i + 1] && (duplicates.isEmpty() || duplicates.get(duplicates.size() - 1) != arrNum[i])) { /* n - 1 veces, ya que el ciclo va
+                a ejecutar las lineas que están dentro de él las vces que se repita el ciclo (n) menos una iteración. Como en este caso el numero de veces que se
+                repite el ciclo es n, entonces las veces que se va a repetir este if son n - 1 veces */
+
+                duplicates.add(arrNum[i]); /*n/2 veces aprox. El peor caso es cuando tenemos una lista asi : [1,1,2,2,3,3,4,4...], ya que de esta manera
+                hacemos que el programa tenga que hacer el mayor número de operaciones, por que el cuerpo del if se ejecutaria n/2 aproximadamente,
+                que es el número más grande de veces que el programa podria ejecutar esta linea. */
             }
         }
 
-        return duplicates.toString();
+        return duplicates; // 1 vez
+
+        /*
+        Suma: 1 + n + n - 1 + n/2 + 1 = 2n + n/2 + 1
+        big O: O(n)
+         */
     }
 
     public boolean palindrome(String word) {
