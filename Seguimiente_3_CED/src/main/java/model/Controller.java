5fcac8fb338bea73c317b5536b5b29292b69c1e7
JuanPaArevalo <juan.parevalo3@gmail.com>
2025-03-17 15:06:43 -0500

    Analisis de todos los algortimos

--- Seguimiente_3_CED/src/main/java/model/Controller.java
+++ Seguimiente_3_CED/src/main/java/model/Controller.java
@@ -22,8 +22,8 @@ public class Controller {
             la condición, entonces se agrega una ultima ejecución a n - 1, es decir, n-1+1 = n. */
 
             if (arrNum[i] == arrNum[i + 1] && (duplicates.isEmpty() || duplicates.get(duplicates.size() - 1) != arrNum[i])) { /* n - 1 veces, ya que el ciclo va
-                a ejecutar las lineas que están dentro de él las vces que se repita el ciclo (n) menos una iteración. Como en este caso el numero de veces que se
-                repite el ciclo es n, entonces las veces que se va a repetir este if son n - 1 veces */
+                a ejecutar las lineas que están dentro de él las veces que se repita el ciclo (n) menos una iteración. Como en este caso el numero de veces que se
+                repite el ciclo (el ciclo "for") es n, entonces las veces que se va a repetir este if son n - 1 veces */
 
                 duplicates.add(arrNum[i]); /*n/2 veces aprox. El peor caso es cuando tenemos una lista asi : [1,1,2,2,3,3,4,4...], ya que de esta manera
                 hacemos que el programa tenga que hacer el mayor número de operaciones, por que el cuerpo del if se ejecutaria n/2 aproximadamente,
@@ -40,23 +40,23 @@ public class Controller {
     }
 
     public boolean palindrome(String word) {
-        int left = 0;
-        int right = word.length() - 1;
+        int left = 0; // Se ejecuta 1 vez
+        int right = word.length() - 1; // Se ejecuta 1 vez
 
-        while (left < right) {
-            if (word.charAt(left) != word.charAt(right)) {
-                return false;
+        while (left < right) { //Se ejecuta n veces
+            if (word.charAt(left) != word.charAt(right)) { // Se ejecutará n - 1 veces. Complejidad O(1)
+                return false; //Se ejecuta n - 1 veces. Complejidad O(1)
             }
-            left++;
-            right--;
+            left++; //Se ejecuta n - 1 veces. Complejidad O(1)
+            right--; //Se ejecuta n - 1 veces. Complejidad O(1)
         }
 
-        return true;
+        return true; //Se ejecuta 1 vez
     }
 
 
     public int bitCounter(String bits){
-        int count = 0;
+        int count = 0; // Se ejecuta 1 vez
         for (char bit : bits.toCharArray()) {   //Complejidad O(n). Depende de la longitud de cadena enviada.
             //Mejor caso = O(n), Peor caso O(n). Siempre recorrerá longitud entera de cadena
             if (bit == '1') {
